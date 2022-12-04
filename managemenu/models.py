f0f92dafc10b4238837b1f0e20ef1d87dad50f7c
Devin Pratista <devinpratista@192.168.1.6>
2022-12-04 07:32:20 +0700

    Managemenu basic flow backend

--- managemenu/models.py
+++ managemenu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class FnB(models.Model):
+
+    CATEGORIES = (
+        ('Food', 'Food'),
+        ('Beverage', 'Beverage')
+    )
+
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    category = models.CharField(null=True, max_length=15, choices=CATEGORIES)
+    description = models.TextField(max_length=150, default='')
+    fnbs = models.Manager()
+
+    class Meta:
+        db_table="fnb"
+
+class Menu(models.Model):
+    menu_id = models.CharField(max_length=30)
+    foods = models.ForeignKey(FnB, on_delete=models.CASCADE, null=True)
--- managemenu/views.py
+++ managemenu/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import FnB, Menu
+from .forms import FnBForm
 
-# Create your views here.
+def index(request):
+    fnb = FnB.fnbs.all()
+    return render(request, "managemenu.html", {"fnb": fnb})
+
+def create(request):
+    if request.method == "POST":
+        form = FnBForm(request.POST)
+        name = request.POST['name']
+
+        if FnB.fnbs.filter(name=name).exists():
+            error_msg = 'This item is already exist'
+            return render(request, 'addfnb.html', {'msg': error_msg})
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect("/managemenu")
+            except:
+                pass
+    else:
+        form = FnBForm()
+        return render(request, "addfnb.html", {'form': form})
+
+def edit(request, id):
+    fnb = FnB.fnbs.get(id = id)
+    return render(request, "editfnb.html", {"fnb": fnb})
+
+def update(request, id):
+    fnb = FnB.fnbs.get(id = id)
+    form = FnBForm(request.POST, instance=fnb)
+    print(form)
+    if form.is_valid():
+        form.save()
+        return redirect("/managemenu")
+
+    return render(request, "editfnb.html", {"fnb": fnb})
+
+def delete(request, id):
+    fnb = FnB.fnbs.get(id = id)
+    fnb.delete()
+    return redirect("/managemenu")
