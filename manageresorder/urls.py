d2c5815c8bdaa14a474be762e900a346364c8b74
Rayhan Adhisworo Jardine Hidayat <rayhan.adhisworo@ui.ac.id>
2022-12-04 18:10:21 +0700

    Done with the basics, part 1.

--- manageresorder/urls.py
+++ manageresorder/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
+
+app_name = 'manageresorder'
 
 urlpatterns = [
-    
+    path('reservations', views.reservation_index, name='index'),
+    path('res_detail/<str:id_res>/', views.read_reservation_detail, name ='res_detail'),
+    path('reservations/cancellation/<str:id_res>', views.reject_reservation, name='reject'),
+    path('reservations/activation/<str:id_res>', views.accept_reservation, name='accept')
 ]
