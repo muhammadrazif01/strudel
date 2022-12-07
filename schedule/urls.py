c24ec70fa19d92e41fde7caee6d92de007907522
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2022-12-07 15:44:48 +0700

    Redoing my model

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import mainpage, create_timeslot
+from .views import mainpage, create_timeslot, show_timeslot
 
 urlpatterns = [
     path('', mainpage, name="mainpage"),
-    path('create-timeslot', create_timeslot, name='create_timeslot')
+    path('create-timeslot', create_timeslot, name='create_timeslot'),
+    path('show-timeslot', show_timeslot, name='show_timeslot'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Timeslot
 from .forms import CreateTimeslotForm
@@ -14,8 +14,24 @@ def create_timeslot(request):
         time_stop = request.POST['time_stop']
         total_seat = request.POST['total_seat']
         seat_availability = request.POST['seat_availability']
+        ts = Timeslot.timeslots.create(
+            day=day,
+            time_start=time_start,
+            time_stop=time_stop,
+            total_seat=total_seat,
+            seat_availability=seat_availability,
+            is_close=False)
+        ts.save()
+        return redirect('/schedule/show-timeslot')
     form = CreateTimeslotForm()    
     data = {
         'form': form,   
     }
     return render(request, "create_timeslot.html", data)
+
+def show_timeslot(request):
+    timeslot = Timeslot.timeslots.all()
+    data = {
+        'list_timeslot': timeslot,
+    }
+    return render(request, "show_timeslot.html", data) 
