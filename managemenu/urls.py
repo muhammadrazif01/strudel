1f51121fe7fca8b4f4cc026018236a9a55115e5b
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2022-12-10 11:43:57 +0700

    Adding Manage Menu feature

--- managemenu/urls.py
+++ managemenu/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import *
 
+app_name = 'managemenu'
 urlpatterns = [
-    
+    path('managemenu/', index),
+    path('managemenu/create', create),
+    path('managemenu/edit/<int:id>', edit),
+    path('managemenu/update/<int:id>', update),
+    path('managemenu/delete/<int:id>', delete),
 ]
